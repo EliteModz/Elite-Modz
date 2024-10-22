de6141f45a2b1f8b46a73503202b97018dc8e528
Elite modz <lmaogaming1213@gmail.com>
2024-10-22 12:45:38 +1100

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,1 +1,30 @@
+let cart = [];
+
+function addToCart(productName, productPrice) {
+    cart.push({ name: productName, price: productPrice });
+    updateCartCount();
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function updateCartCount() {
+    document.getElementById('cart-count').innerText = cart.length;
+}
+
+function displayCartItems() {
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    let cartTotal = 0;
+    const cartElement = document.getElementById('cart-items');
+
+    cartItems.forEach(item => {
+        cartElement.innerHTML += `<p>${item.name} - $${item.price}</p>`;
+        cartTotal += item.price;
+    });
+
+    document.getElementById('cart-total').innerText = cartTotal;
+}
+
+// Call on cart.html
+if (document.getElementById('cart-items')) {
+    displayCartItems();
+}
 
