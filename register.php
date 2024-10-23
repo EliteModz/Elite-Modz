17df21333405668126e46b2ad8e7ce17bf780295
Elite modz <lmaogaming1213@gmail.com>
2024-10-23 20:30:11 +1100

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-$servernme = "localhost"; // Your database server
+$servername = "localhost"; // Your database server
 $username = "root"; // Your database username
 $password = ""; // Your database password
 $dbname = "your_database"; // Your database name
