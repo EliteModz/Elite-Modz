08740f9dbd269822e21c44dcc39296781efd5bcf
Elite modz <lmaogaming1213@gmail.com>
2024-10-29 13:04:38 +1100

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,6 +1,5 @@
-// Function to add an item to the cart
 function addToCart(item) {
-  let cart = JSON.parse(localStorage.getItem('cartItems')) || [];
-  cart.push(item);
-  localStorage.setItem('cartItems', JSON.stringify(cart));
+    let cart = JSON.parse(localStorage.getItem('cartItems')) || [];
+    cart.push(item);
+    localStorage.setItem('cartItems', JSON.stringify(cart));
 }
